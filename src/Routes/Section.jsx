e297f5add39130894d8be45043408706939f764f
Nathashaperera42 <shashininathasha42@gmail.com>
2025-02-27 22:56:19 +0530

    update

--- src/Routes/Section.jsx
+++ src/Routes/Section.jsx
@@ -2,13 +2,17 @@ import React, { lazy, Suspense } from "react";
 import { Outlet, useRoutes } from "react-router-dom";
 import Layout from "../Layout/Nav-Bar/NavBar";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Signup from "../Pages/Signup/Signup";
 
 
 function Router() {
+  
   const routes = useRoutes([
     {
       path: "/",
       element: (
+     
         <Layout>
           <Suspense>
             <Outlet />
@@ -18,8 +22,12 @@ function Router() {
       children: [ 
         { path: "/", element: <Home /> },
        
+
+       
       ],
     },
+    { path: "/login", element: < Login/> },
+    { path: "/signup", element: < Signup/> },
   ]);
 
   return routes;
