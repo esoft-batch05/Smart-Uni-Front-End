4274bda18f44df9bf4410874216f18343ea9f49d
Sunera_s <sunera719@gmail.com>
2025-03-21 14:18:24 +0530

    ..

--- src/app/apiManager.js
+++ src/app/apiManager.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "../app/store"; 
 import { setTokens, clearTokens } from "../Reducers/authSlice";
 
-const API_BASE_URL = "http://localhost:5005/api";
+const API_BASE_URL = "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
